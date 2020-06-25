e63879294e723f37b743ea29624067d724578a79
utukaule <utuvines@gmail.com>
2020-06-25 18:52:03 +0530

    Second commit

--- my_todo/my_todo/forms.py
+++ my_todo/my_todo/forms.py
@@ -4,6 +4,7 @@ from django.forms import ModelForm
 from .models import *
 
 class My_todoForm(forms.ModelForm):
+	title = forms.CharField(widget= forms.TextInput(attrs={'placeholder':'Add new task'}))
 	class Meta:
 		model = My_todo
 		fields = '__all__'
